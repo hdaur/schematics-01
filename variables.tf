dbf1bda11d19239eb30e8efdd6fd3b266066fee3
hdaur <harald.daur@de.ibm.com>
2017-07-14 09:29:09 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,3 +4,9 @@ variable "myparm1" {
   description = "A parameter to pass to the template."
   default = "DefaultHello"
 }
+
+variable "myparm_sensitive" {
+  description = "A parameter that contains sensitive info such as password"
+  default = "123456789"
+}
+
